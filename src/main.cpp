d5d2f6fa134ceafd4b61f7bf9069485b09051c7e
AwAis goat <ashrafawais915@gmail.com>
2026-09-02 20:08:43 +0500

    useless commit

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,13 @@
 
 int main(int argc, char *argv[])
 {
+
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
     BACKEND upper_controls;
     TEMPRATURE_CONTROLS temp_unit;
     MusicController music_controller;
+    //next compelete search bar fully
     engine.rootContext()->setContextProperty("upper_control", &upper_controls);
     engine.rootContext()->setContextProperty("Temprature_Controls", &temp_unit);
     engine.rootContext()->setContextProperty("musicController", &music_controller);
